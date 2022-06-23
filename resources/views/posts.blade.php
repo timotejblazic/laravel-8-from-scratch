06722842bad919ea6c4a7afb51d0ddad613891b9
Timotej Blazic <timotej.blazic@spletna-postaja.com>
2022-06-23 13:56:39 +0200

    Episode 11

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,30 +4,14 @@
 <link rel="stylesheet" href="/app.css">
 
 <body>
-    <article>
-        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+    <?php foreach ($posts as $post) : ?>
 
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet ante ac nulla consectetur ultricies. Nullam vel elit ultrices, imperdiet tellus eu, maximus risus. In magna augue, dictum nec leo et, gravida auctor odio. Ut eu tortor ut ex consequat ultrices. Nam molestie ornare nibh in viverra. Nulla vestibulum molestie justo, eget finibus odio rhoncus vitae. Sed elit libero, tincidunt quis lacinia id, malesuada et tellus. In varius convallis lorem, ac congue tortor pretium ut. Donec elementum ligula nulla, finibus ultricies mi iaculis id. Morbi blandit laoreet arcu et interdum. Cras viverra eros quis aliquam molestie. Duis porta eget dolor id commodo.
-        </p>
+        <article>
+            <?= $post; ?>
 
-    </article>
+        </article>
 
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+    <?php endforeach; ?>
 
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet ante ac nulla consectetur ultricies. Nullam vel elit ultrices, imperdiet tellus eu, maximus risus. In magna augue, dictum nec leo et, gravida auctor odio. Ut eu tortor ut ex consequat ultrices. Nam molestie ornare nibh in viverra. Nulla vestibulum molestie justo, eget finibus odio rhoncus vitae. Sed elit libero, tincidunt quis lacinia id, malesuada et tellus. In varius convallis lorem, ac congue tortor pretium ut. Donec elementum ligula nulla, finibus ultricies mi iaculis id. Morbi blandit laoreet arcu et interdum. Cras viverra eros quis aliquam molestie. Duis porta eget dolor id commodo.
-        </p>
-
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet ante ac nulla consectetur ultricies. Nullam vel elit ultrices, imperdiet tellus eu, maximus risus. In magna augue, dictum nec leo et, gravida auctor odio. Ut eu tortor ut ex consequat ultrices. Nam molestie ornare nibh in viverra. Nulla vestibulum molestie justo, eget finibus odio rhoncus vitae. Sed elit libero, tincidunt quis lacinia id, malesuada et tellus. In varius convallis lorem, ac congue tortor pretium ut. Donec elementum ligula nulla, finibus ultricies mi iaculis id. Morbi blandit laoreet arcu et interdum. Cras viverra eros quis aliquam molestie. Duis porta eget dolor id commodo.
-        </p>
-
-    </article>
+    
 </body>
