c4d88f8aefdcb9a7d10e5d5d4f318d853af6f649
Timotej Blazic <timotej.blazic@spletna-postaja.com>
2022-06-23 15:19:19 +0200

    Episode 12

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,21 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post {
-    public static function all() {
-        $files = File::files(resource_path("posts/"));
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+    public $slug;
 
-        return array_map(fn($file) => $file->getContents(), $files);
+    public function __construct($title, $excerpt, $date, $body, $slug) {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+
+    public static function all() {
+        return collect(File::files(resource_path("posts")))
+            ->map(fn($file) => YamlFrontMatter::parseFile($file))
+            ->map(fn($document) => new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+            ));
     }
 
     public static function find($slug) {
-        if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
-            // abort(404)
-            throw new ModelNotFoundExpection();
-        }
-
-        return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
+        // returns the post where metadata slug matches $slug
+        return static::all()->firstWhere('slug', $slug);
     }
 
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,8 +7,15 @@
     <?php foreach ($posts as $post) : ?>
 
         <article>
-            <?= $post; ?>
+            <h1>
+                <a href="/posts/<?= $post->slug; ?>">
+                    <?= $post->title; ?>
+                </a>
+            </h1>
 
+            <div>
+                <?= $post->excerpt; ?>
+            </div>
         </article>
 
     <?php endforeach; ?>
