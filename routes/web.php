9f3453d13c1dc9e8b123740fdb2de73aa4dc5f4e
Timotej Blazic <timotej.blazic@spletna-postaja.com>
2022-06-29 12:43:01 +0200

    Episode 25

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,5 +25,11 @@ Route::get('/', function () {
 Route::get('posts/{post:slug}', function (Post $post) {  // Post::where('slug', $post)->firstOrfail();
     return view('post', [
         'post' => $post
+    ]);
+});
+
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
     ]);
 });
