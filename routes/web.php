e8a3bb4469ff2e4491bd6a8d129b7a1c1c164aac
Timotej Blazic <timotej.blazic@spletna-postaja.com>
2022-07-12 16:23:54 +0200

    Episode 34

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::latest()->get()
+        'posts' => Post::latest()->get(),
+        'categories' => Category::all()
     ]);
 });
 
@@ -31,12 +32,15 @@ Route::get('posts/{post:slug}', function (Post $post) {  // Post::where('slug', 
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ]);
 });
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts,
+        'categories' => Category::all()
     ]);
 });
