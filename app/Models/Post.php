4148f8f1fab3d82abe0f93e42cc574f42034f92e
Timotej Blazic <timotej.blazic@spletna-postaja.com>
2022-07-18 16:42:09 +0200

    Episode 39

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -22,17 +22,16 @@ class Post extends Model
         return 'slug';
     }*/
 
-    public function scopeFilter($query, array $filters) {  // Post::newQuery()->filter()
-//        if ($filters['search'] ?? false) {
-//            $query
-//                ->where('title', 'like', '%' . request('search') . '%')
-//                ->orWhere('body', 'like', '%' . request('search') . '%');
-//        }
-
+    public function scopeFilter($query, array $filters) {
         $query->when($filters['search'] ?? false, fn ($query, $search) =>
             $query
                 ->where('title', 'like', '%' . $search . '%')
                 ->orWhere('body', 'like', '%' . $search . '%'));
+
+        $query->when($filters['category'] ?? false, fn ($query, $category) =>
+            $query->whereHas('category', fn($query) =>
+                $query->where('slug', $category))
+        );
     }
 
     public function category() {
