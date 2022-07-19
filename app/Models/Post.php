e3e8a83cbc876cc0dfb15e7b4b9ea43e1f778d80
Timotej Blazic <timotej.blazic@spletna-postaja.com>
2022-07-19 11:52:58 +0200

    Episode 43

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -24,9 +24,11 @@ class Post extends Model
 
     public function scopeFilter($query, array $filters) {
         $query->when($filters['search'] ?? false, fn ($query, $search) =>
-            $query
-                ->where('title', 'like', '%' . $search . '%')
-                ->orWhere('body', 'like', '%' . $search . '%'));
+            $query->where(fn($query) =>
+                $query->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%')
+            )
+        );
 
         $query->when($filters['category'] ?? false, fn ($query, $category) =>
             $query->whereHas('category', fn($query) =>
@@ -34,8 +36,8 @@ class Post extends Model
         );
 
         $query->when($filters['author'] ?? false, fn ($query, $author) =>
-        $query->whereHas('author', fn($query) =>
-        $query->where('username', $author))
+            $query->whereHas('author', fn($query) =>
+                $query->where('username', $author))
         );
     }
 
