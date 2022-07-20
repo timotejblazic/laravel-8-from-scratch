96de6c1ddb62530645945672772a5d9e91eb8ae5
Timotej Blazic <timotej.blazic@spletna-postaja.com>
2022-07-20 13:13:19 +0200

    Episode 54 - Dynamic comments

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -41,6 +41,10 @@ class Post extends Model
         );
     }
 
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
+
     public function category() {
         // hasOne, hasMany, belongsTo, belongsToMany
         return $this->belongsTo(Category::class);
