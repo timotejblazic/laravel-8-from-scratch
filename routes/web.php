5afb71ad6f0d664d618e243bac1a82d0aabd4b35
Timotej Blazic <timotej.blazic@spletna-postaja.com>
2022-07-21 08:43:53 +0200

    Episode 58 - Started Mailchimp

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,23 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('ping', function () {
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us17'
+    ]);
+
+    $response = $mailchimp->lists->addListMember('e8f310c176', [
+        'email_address' => 'fake.email@gmail.com',
+        'status' => 'subscribed'
+    ]);
+
+    ddd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
