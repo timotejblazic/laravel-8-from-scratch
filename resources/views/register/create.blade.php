0e800a479c4abf50c483c41b095e19943801475a
Timotej Blazic <timotej.blazic@spletna-postaja.com>
2022-07-21 16:02:19 +0200

    Episode 66

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -6,90 +6,12 @@
             <form method="POST" action="/register" class="mt-10">
                 @csrf
 
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="name"
-                    >
-                        Name
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="text"
-                           name="name"
-                           id="name"
-                           value="{{ old('name') }}"
-                           required
-                    >
+                <x-form.input name="name" type="text" />
+                <x-form.input name="username" type="text" />
+                <x-form.input name="email" type="text" />
+                <x-form.input name="password" type="text" />
 
-                    @error('name')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="username"
-                    >
-                        Username
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="text"
-                           name="username"
-                           id="username"
-                           value="{{ old('username') }}"
-                           required
-                    >
-
-                    @error('username')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="email"
-                    >
-                        Email
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="email"
-                           name="email"
-                           id="email"
-                           value="{{ old('email') }}"
-                           required
-                    >
-
-                    @error('email')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
-                           for="password"
-                    >
-                        Password
-                    </label>
-                    <input class="border border-gray-400 p-2 w-full"
-                           type="password"
-                           name="password"
-                           id="password"
-                           required
-                    >
-
-                    @error('password')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-6">
-                    <button type="submit"
-                            class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500"
-                    >
-                        Submit
-                    </button>
-                </div>
-
-
+                <x-form.button>Register</x-form.button>
             </form>
         </main>
     </section>
