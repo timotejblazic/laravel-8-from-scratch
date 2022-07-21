7cc5624120aa5b1f57e5af24ed5cf3ab3d5bac55
Timotej Blazic <timotej.blazic@spletna-postaja.com>
2022-07-21 13:04:59 +0200

    Episode 62 - Access For Admins

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller
 {
@@ -20,5 +21,9 @@ class PostController extends Controller
         return view('posts.show', [
             'post' => $post
         ]);
+    }
+
+    public function create() {
+        return view('posts.create');
     }
 }
